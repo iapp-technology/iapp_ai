6c6375c8134530a3de42e3a0c3870ee3b1301933
Kobkrit Viriyayudhakorn <kobkrit@iapp.co.th>
2022-05-17 16:16:52 +0700

    OK Simple variable name

--- iapp_ai/module_api.py
+++ iapp_ai/module_api.py
@@ -5,17 +5,17 @@ class api():
     def __init__(self, apikey):
         self.apikey = apikey
     
-    def idcard_front(self, file_path, additional_headers={}, additional_data_payload={}, additional_files=[]):
-        headers = {"apikey":self.apikey, **additional_headers}
-        data_payload = {**additional_data_payload}
-        files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
-        files.extend(additional_files)
+    def idcard_front(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
+        request_files.extend(files)
 
-        print(headers)
-        print(data_payload)
-        print(files)
+        print(request_headers)
+        print(request_data_payload)
+        print(request_files)
         
-        response = requests.request("POST", "https://api.iapp.co.th/thai-national-id-card/v3/front", headers=headers, data=data_payload, files=files)
+        response = requests.request("POST", "https://api.iapp.co.th/thai-national-id-card/v3/front", headers=request_headers, data=request_data_payload, files=request_files)
         print(response.json())
         return response.json()  #Return as the Object
 
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
+#Example
+
 import iapp_ai
 api = iapp_ai.api("iapp-website")
+#Normal usage
 print(api.idcard_front("media/id-card-front.jpg"))
 print("----")
-print(api.idcard_front("media/id-card-front.jpg", additional_headers={"apikey":"123"}))
+#Change apikey to the invalid one.
+print(api.idcard_front("media/id-card-front.jpg", headers={"apikey":"123"}))
