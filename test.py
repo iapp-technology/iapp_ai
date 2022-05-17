931c3c1929e043d1b7c6122bd27f31e15eb14878
Kobkrit Viriyayudhakorn <kobkrit@iapp.co.th>
2022-05-17 16:14:16 +0700

    Support headers configuration

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import iapp_ai
 api = iapp_ai.api("iapp-website")
 print(api.idcard_front("media/id-card-front.jpg"))
+print("----")
 print(api.idcard_front("media/id-card-front.jpg", additional_headers={"apikey":"123"}))
