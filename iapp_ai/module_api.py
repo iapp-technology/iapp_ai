a95bcd5dad7ead4144da799a6cd2f01088459777
Rose <sreyroth.phe@gmail.com>
2022-05-17 17:53:05 +0700

    Update APIs

--- iapp_ai/module_api.py
+++ iapp_ai/module_api.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class api():
     apikey = ""
@@ -19,3 +20,101 @@ class api():
         print(response.json())
         return response.json()  #Return as the Object
 
+    def idcard_back(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
+        request_files.extend(files)
+        
+        response = requests.request("POST", "https://api.iapp.co.th/thai-national-id-card/v3/back", headers=request_headers, data=request_data_payload, files=request_files)
+        print(response.json())
+        return response.json()  #Return as the Object
+
+    def book_bank_api(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
+        request_files.extend(files)
+        
+        response = requests.request("POST", "https://api.iapp.co.th/book-bank-ocr/file", headers=request_headers, data=request_data_payload, files=request_files)
+        print(response.text)
+        # return response.json()  #Return as the Object
+
+    def face_liveness(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
+        request_files.extend(files)
+        
+        response = requests.request("POST", "https://api.iapp.co.th/passive-face-liveness-detection", headers=request_headers, data=request_data_payload, files=request_files)
+        print(response.json())
+
+        return response.json()  #Return as the Object
+    def info_face_liveness(self, headers={}, taskGuid={}):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_url = "https://api.iapp.co.th/passive-face-liveness-detection/" + taskGuid
+
+        response = requests.request("GET", request_url, headers=request_headers)
+        print(response.json())
+        return response.json()  #Return as the Object
+    
+    def signature_detect(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
+        request_files.extend(files)
+        
+        response = requests.request("POST", "https://api.iapp.co.th/book-bank-ocr/file", headers=request_headers, data=request_data_payload, files=request_files)
+        print(response.text)
+        # return response.json() #Return as the Object
+
+    def water_meter_binary(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'image/jpg'))]
+        request_files.extend(files)
+        
+        response = requests.request("POST", "https://api.iapp.co.th/meter-number-ocr/file", headers=request_headers, data=request_data_payload, files=request_files)
+        print(response.text)
+        return response.text  #Return as the Object
+     
+
+    def thai_qa_api(self, headers={}, question= {}, document={}):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = json.dumps({
+            'question': question,
+            'document': document})
+
+        response = requests.request("POST", "https://api.iapp.co.th/thai-qa/inference", headers=request_headers, data=request_data_payload)
+        print(response.text)
+        return response.text
+    
+    def thai_asr_api(self, file_path, headers={}, data_payload={}, files=[]):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_files = [('file',(file_path, open(file_path,'rb'),'audio/mpga'))]
+        request_files.extend(files)
+        
+        response = requests.request("POST", "https://api.iapp.co.th/asr", headers=request_headers, data=request_data_payload, files=request_files)
+        print(response.json())
+        return response.json()  #Return as the Object
+
+    def thai_thaitts_kaitom(self, text={},headers={}, data_payload={} ):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_url = "https://api.iapp.co.th/thai-tts-kaitom/tts?text=" + text
+
+        response = requests.request("GET", request_url, headers=request_headers, data=request_data_payload)
+        print(response.text)
+        return response.text
+
+    def thai_thaitts_cee(self, text={},headers={}, data_payload={} ):
+        request_headers = {"apikey":self.apikey, **headers}
+        request_data_payload = {**data_payload}
+        request_url = "https://api.iapp.co.th/thai-tts-cee/tts?text=" + text
+
+        response = requests.request("GET", request_url, headers=request_headers, data=request_data_payload)
+        print(response.text)
+        return response.text
+        
+        
--- test.py
+++ test.py
@@ -1,9 +1,29 @@
+# -*- coding: utf-8 -*-
 #Example
 
 import iapp_ai
 api = iapp_ai.api("iapp-website")
 #Normal usage
-print(api.idcard_front("media/id-card-front.jpg"))
+api.idcard_front("media/id-card-front.jpg")
+print("----")
+api.idcard_back("media/id-card-back.jpg")
+print("----")
+api.book_bank_api("media/E7393203-15.jpg")
+print("----")
+taskGuid = api.face_liveness("media/passport.jpg")
+api.info_face_liveness(taskGuid=str(taskGuid))
+print("----")
+api.signature_detect("media/signature.jpg")
+print("----")
+api.water_meter_binary("media/wm.jpg")
+print("----")
+api.thai_qa_api(headers={'Content-Type': 'application/json'}, question="แค็วม์เป็นแค่หมู่บ้านใช่หรือไม่", document="จังหวัดแค็วม์โปแลนด์คือหน่วยการปกครองท้องถิ่นของประเทศโปแลนด์ในช่วงปี ค.ศ.1975 - ค.ศ.1998 จังหวัดได้รับการรวมเข้ากับจังหวัดลูบลินมีเมืองหลักคือแค็วม์ใน ปี ค.ศ.1998 มีพื้นที่ประมาณ 3865 ตารางกิโลเมตรและมีประชากร 248800 คน")
+print("----")
+api.thai_asr_api("media/data.wav")
+print("----")
+api.thai_thaitts_kaitom("Hello")
+print("----")
+api.thai_thaitts_cee("Hello")
 print("----")
 #Change apikey to the invalid one.
-print(api.idcard_front("media/id-card-front.jpg", headers={"apikey":"123"}))
+# print(api.idcard_front("media/id-card-front.jpg", headers={"apikey":"123"}))
