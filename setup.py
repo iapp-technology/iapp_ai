f47ae1bf568824b2cd7279b3fe3529d4151c4839
Kobkrit Viriyayudhakorn <kobkrit@iapp.co.th>
2023-01-20 16:55:20 +0700

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/iapp-technology/iapp_ai',
-    version='version='0.1.0'',
+    version='version='0.2.0'',
     zip_safe=False,
 )
