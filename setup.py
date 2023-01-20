209d0b84d9dcfbd1c3bd5f3cf14f8399329b595b
Kobkrit Viriyayudhakorn <kobkrit@iapp.co.th>
2023-01-20 17:15:42 +0700

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
-    # long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords='iapp_ai',
     name='iapp_ai',
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/iapp-technology/iapp_ai',
-    version='version='1.2.0'',
+    version='1.2.0',
     zip_safe=False,
 )
